63a308318ba215e389d0b647bc76af32e599e5b5
ArthurGLCout <gorgesarthur8@gmail.com>
2025-09-23 13:57:08 -0300

    Add files via upload

---
+++ ARRAYS.js
@@ -0,0 +1,22 @@
+/* ARRAYS 
+valores em uma variavel. diferente dos 00] que funcionam 
+com propriedades ou "nomes" que você der os itens dentro dele,
+os arrays não possuem propriedades que tem dentro dele 
+é encontrado pela posição 
+
+imagine um array como uma lista de itens, com colunas e ordem de posição dentro dele. 
+
+
+forma 1:
+ex: const lista = ["arroz", "feijão","macarrão"]
+forma 2: 
+const lista = [
+     "arroz"   ,  [0]
+     "feijão"  ,  [1] 
+     "macarrão",  [2] 
+     "leite"   ,  [3]
+ 
+]
+
+
+*/
