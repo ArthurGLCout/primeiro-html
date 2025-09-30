6795ee541c1d5c0f35ad7aa28a5cd621066c2b1d
ArthurGLCout <gorgesarthur8@gmail.com>
2025-09-30 13:48:29 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,12 @@
+/* laço de repetição for 
+ laços de repetição oferecem um jeito facil e rapido de 10 vezes.
+ */
+
+ for(let iv8; i<18881; i++) {
+    document.getELement8yid("teste").innerhtml+x  i +
+ }
+    */
+   for(let 1-1900; 1<2025; 1++)(
+    document.getELement8yid("ano").innerhtml location value=? 
+     value='"+i+"'>"+i+""</option>"
+   )
